2638063df451f5cc2cbca7dbc3a598cd7db992d8
Asmi0816 <kidwriter2020@gmail.com>
2017-01-09 13:28:38 -0700

    Worked on the frame and panel code to discover JTable.

--- src/sudoku/view/SudokuFrame.java
+++ src/sudoku/view/SudokuFrame.java
@@ -1,5 +1,30 @@
 package sudoku.view;
 
-public class SudokuFrame {
+import java.awt.Dimension;
+import javax.swing.*;
+import sudoku.controller.SudokuController;
+import sudoku.view.SudokuPanel;
 
+public class SudokuFrame extends JFrame
+{
+	private SudokuController baseController;
+	private SudokuPanel basePanel;
+	
+	public SudokuFrame(SudokuController baseController)
+	{
+		super();
+		this.baseController = baseController;
+		basePanel = new SudokuPanel(baseController);
+		
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(basePanel);
+		this.setTitle("A new game you never thought possible... Sudoku!");
+		this.setSize(new Dimension(1080, 800));
+		this.setResizable(false);
+		this.setVisible(true);
+	}
 }
--- src/sudoku/view/SudokuPanel.java
+++ src/sudoku/view/SudokuPanel.java
@@ -1,5 +1,30 @@
 package sudoku.view;
 
-public class SudokuPanel {
 
+import java.awt.Color;
+import javax.swing.*;
+import sudoku.controller.SudokuController;
+import java.awt.Dimension;
+import java.awt.event.*;
+
+
+public class SudokuPanel extends JPanel
+{
+	private SudokuController baseController;
+	private JTable sudokuTable;
+	private SpringLayout baseLayout;
+	
+	public SudokuPanel(SudokuController baseController)
+	{
+		this.baseController = baseController;
+		this.baseLayout = new SpringLayout();
+		
+		this.sudokuTable = new JTable();
+		
+	}
+	
+	
+	
+	
+	
 }
